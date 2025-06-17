6a5976d147e30b44d3b3f2c59d33ae60488135ce
phong <phongit1995@gmail.com>
2025-06-17 22:57:20 +0700

    update cicd

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import App from './app';
+import App from './home';
 import { routesSection } from './routes/sections';
 import { ErrorBoundary } from './routes/components';
 
